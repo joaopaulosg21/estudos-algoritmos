e6a0fe7a692771ee8f9948420650e518f9ae812a
joaopaulo <joaopaulosg17@gmail.com>
2023-11-08 13:10:38 -0300

    refactor: Atualização no SelectionSort

--- sort/SelectionSort.java
+++ sort/SelectionSort.java
@@ -1,29 +1,38 @@
 package sort;
+
 import java.util.Arrays;
+
+import utils.Utils;
 
 public class SelectionSort {
 
     public static void main(String[] args) {
-        int[] arr = {3,23,53,32,14,21,12,84,56,45};
-        sort(arr);
+        int[] arr = Utils.generateRandom(10);
+
         System.out.println(Arrays.toString(arr));
-    } 
+
+        sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
 
     private static void sort(int[] arr) {
 
-        for(int i=0; i < arr.length-1;i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
 
             int menorIndice = i;
 
-            for(int j=i; j < arr.length;j++) {
-                if(arr[j] < arr[menorIndice]) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] < arr[menorIndice]) {
                     menorIndice = j;
                 }
             }
 
-            int temp = arr[i];
-            arr[i] = arr[menorIndice];
-            arr[menorIndice] = temp;
+            if (i != menorIndice) {
+                int temp = arr[i];
+                arr[i] = arr[menorIndice];
+                arr[menorIndice] = temp;
+            }
         }
     }
 }
